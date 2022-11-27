e6c31cd39aaac29f8ab52469bc69cc7e912cbd1b
Alejandra De Los Santos <aledeloss@gmail.com>
2022-11-27 12:31:47 -0300

    Add new recipe page

---
+++ pages/recipe/new.tsx
@@ -0,0 +1,12 @@
+import NewRecipeForm from '../../components/NewRecipeForm';
+
+const NewRecipe = () => {
+  return (
+    <>
+      <h1>Add new recipe</h1>
+      <NewRecipeForm />
+    </>
+  );
+};
+
+export default NewRecipe;

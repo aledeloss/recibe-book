983fab0dcb4b87ad6b5296500f845e141a65dfe4
Alejandra De Los Santos <aledeloss@gmail.com>
2022-11-27 23:08:39 -0300

    Add colors and layout styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 
 const Layout = ({ children }: { children: JSX.Element }) => {
   return (
-    <>
-      <div>Navbar</div>
-      <main>{children}</main>
-      <div>Footer</div>
-    </>
+    <div className='w-screen'>
+      <header className='bg-orange h-24 flex justify-center items-center'>
+        My recipe book
+      </header>
+      <main className='p-4 bg-yellow'>{children}</main>
+      <footer className='bg-orange h-12 flex justify-center items-center absolute bottom-0 w-full'>
+        <span>
+          Made by{' '}
+          <a
+            href='https://github.com/aledeloss'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Ale Delos
+          </a>
+        </span>
+      </footer>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    colors: {
+      'pr-blue': '#23395b',
+      turquoise: '#B9E3C6',
+      green: '#2d8b6d',
+      ruby: '#D81E5B',
+      canary: '#FFFD98',
+      blue: '#1fb6ff',
+      purple: '#7e5bef',
+      orange: '#ff7849',
+      yellow: '#ffc82c',
+      'gray-dark': '#273444'
+    },
     extend: {}
   },
   plugins: []

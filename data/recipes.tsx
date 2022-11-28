53886cf3c92ad02656356452d946d45e564f7a4d
Alejandra De Los Santos <aledeloss@gmail.com>
2022-11-27 23:09:56 -0300

    Add very basic recipe detail

---
+++ data/recipes.tsx
@@ -0,0 +1,28 @@
+export const recipes = [
+  {
+    id: 'bizcochos-de-oregano',
+    title: 'Bizcochos de orégano',
+    ingredients: [
+      {
+        name: 'harina leudante',
+        quantity: 400,
+        unity: 'grs.'
+      },
+      {
+        name: 'sal, pimienta y orégano'
+      },
+      {
+        name: 'aceite',
+        quantity: 100,
+        unity: 'ml.'
+      },
+      {
+        name: 'agua',
+        quantity: 50,
+        unity: 'ml.'
+      }
+    ],
+    directions:
+      'Mezclar bien todo en un bowl, hacer los bizcochitos (como bolitas aplastadas o cortados con molde). Llevar a horno suave en una placa por 7/10 minutos por lado.'
+  }
+];

1e4da7fc9e690891d7a7707ce50c2b75a90d41cb
Alejandra De Los Santos <aledeloss@gmail.com>
2022-11-28 19:44:23 -0300

    Change files extension

--- __tests__/Layout.test.tsx
+++ __tests__/Layout.test.tsx
@@ -3,18 +3,22 @@ import Layout from '../components/Layout';
 import '@testing-library/jest-dom';
 
 describe('Layout', () => {
+  beforeEach(() => {
+    render(
+      <Layout>
+        <>Children</>
+      </Layout>
+    );
+  });
   it('Renders a header', () => {
-    render(<Layout />);
     const header = screen.getByText('My recipe book');
     expect(header).toBeInTheDocument();
   });
   it('Renders a footer', () => {
-    render(<Layout />);
     const footer = screen.getByText('Ale Delos');
     expect(footer).toBeInTheDocument();
   });
   it('Renders a main section', () => {
-    render(<Layout />);
     const footer = screen.getByRole('main');
     expect(footer).toBeInTheDocument();
   });

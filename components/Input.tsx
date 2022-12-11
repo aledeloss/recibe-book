7356df15cac692808eebcf0f349b5328b49765c3
Alejandra De Los Santos <aledeloss@gmail.com>
2022-12-11 14:30:15 -0300

    Add error message

--- components/Input.tsx
+++ components/Input.tsx
@@ -30,14 +30,16 @@ export function Input({
   return (
     <div className='mb-4' style={style && style}>
       <label className='flex flex-col items-start '>
-        <span className={'block'}>{`${label}${isRequired ? ' *' : ''}`}</span>
+        <span className={'block'}>{`${label}${isRequired ? '*' : ''}`}</span>
         <InputTag
           className={`appearance-none w-full py-2 px-3 mt-1 leading-tight border border-bottom bg-yellow outline-none focus:shadow-outline ${
             error && 'border-red-500'
           }`}
           disabled={isDisabled}
           type={type}
-          {...register(name, { required: isRequired })}
+          {...register(name, {
+            required: { value: isRequired, message: 'This field is required' }
+          })}
         />
       </label>
       {error && (

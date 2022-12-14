f2c0f9ad587adccc98c1289e4b2470c8bb072ded
Alejandra De Los Santos <aledeloss@gmail.com>
2022-12-14 20:18:04 -0300

    Remove unsed hook

--- pages/recipes/detail/[id].tsx
+++ pages/recipes/detail/[id].tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../app/store';
 import { recipe } from '../../../types/recipe';
+import Button from '../../../components/Button';
+import { deleteRecipe } from '../../../slicers/recipesSlicer';
 
 const Detail = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
   const myRecipes: recipe[] = useSelector(
     (state: RootState) => state.recipes.value
   );
@@ -29,6 +32,13 @@ const Detail = () => {
           </ul>
           <h3 className='text-xl mt-4'>Directions</h3>
           <div>{currentRecipe.directions}</div>
+          <Button
+            label='Remove recipe'
+            handleClick={() => {
+              if (typeof id === 'string') dispatch(deleteRecipe(id));
+              router.push('/');
+            }}
+          />
         </>
       )}
     </>
